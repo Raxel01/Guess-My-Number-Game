37225af0cd1c87bf9c664738473021e378a7b202
Abdelali Ait Talb <abait-ta@e2r9p6.1337.ma>
2024-05-18 13:28:46 +0100

    - Include The game -

--- README.md
+++ README.md
@@ -5,4 +5,23 @@ Combining the Power of HTML CSS JAVASCRIPT to Crete a Funny game called Guest my
 
 
 
-You think you can Beat Me at My Game ? clone the Game and Send Me your Winning screenshoot to get Your Prize ! 
+You think you can Beat Me at My Game ? clone the Game and Send Me your Winning screenshoot to get Your Prize !
+
+Here is the game photo : 
+
+<img src="./img/mygame.png">
+
+If you see the green Background Congratualtion you Win Me :
+
+<img src="./img/win.png">
+
+This is For Losers haha :
+<img src="./img/lose.png">
+
+You can say that you can't beat me using Hints :
+
+<img src="./img/Hints.png">
+
+Displayd in a red border and give you where the Tarhet number is located.
+
+
---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const random_n =  Math.trunc( Math.random() * 20 ) + 1;
+
+document.querySelector('.check').addEventListener('click', function(){
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('h1').textContent = 'Guess My Number!';
+    document.querySelector('h1').style.border = 'none';
+    const Numberguess = Number(document.querySelector('.guess').value);
+    if (!Numberguess || Numberguess < 0){
+        document.querySelector('.message').textContent = 'Invalid Input number !';
+    }
+    else{
+        if  (Number(document.querySelector('.score').textContent) - 1 >= 0){
+        if (Numberguess > random_n){
+            document.querySelector('.message').textContent = 'Your Number is to High Retry !';
+                document.querySelector('.score').textContent   = Number(document.querySelector('.score').textContent) - 1;
+        }
+        else if (Numberguess < random_n){
+                document.querySelector('.message').textContent = 'Your Number is to Low Retry !';
+                document.querySelector('.score').textContent   = Number(document.querySelector('.score').textContent) - 1;
+            }
+        else{
+            document.querySelector('.message').textContent = 'winner winner chicken dinner !'.toUpperCase();
+            document.querySelector('body').style.backgroundColor = '#60b347';
+            document.querySelector('.number').style.width = '90rem';
+            document.querySelector('.number').textContent = random_n;
+        }
+    }
+    else{
+        document.querySelector('.message').textContent = 'Your Score is 0 you can\'t  Play Now !';
+        document.querySelector('body').style.backgroundColor = '#ff073a';
+        document.querySelector('h1').textContent = 'Loser ! Press \'Again\'';
+    }
+    }
+})
+
+document.querySelector('.again').addEventListener('click', function(){
+    document.querySelector('body').style.backgroundColor = '#515151';
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent   = 20;
+    document.querySelector('h1').textContent = 'Guess My Number!';
+    document.querySelector('h1').style.border = 'none';
+    document.querySelector('.guess').value = '';
+    document.querySelector('.number').textContent = '?';
+})
+
+document.querySelector('.Hint').addEventListener('click', function(){
+    document.querySelector('h1').textContent = `The Secret Number is beetwen ${random_n - 4} and ${random_n +3}`;
+    document.querySelector('h1').style.fontSize = '2rem';
+    document.querySelector('h1').style.border = '1px solid #ff073a';
+})
